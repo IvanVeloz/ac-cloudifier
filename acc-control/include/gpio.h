f3a699a19ac88cb01c75c2bbca4e05d932b2384e
Ivan Veloz <devel@ivanveloz.com>
2024-05-23 16:25:22 +0000

    Add GPIO control class

---
+++ acc-control/include/gpio.h
@@ -0,0 +1,71 @@
+/* gpio.h is the library for control of the GPIO in the ac-cloudifier project.
+ * Thanks to pigpio, multiple instances of this library can be used at the same.
+ */
+
+#ifndef _GPIO_H_
+#define _GPIO_H_
+
+#include <pigpiod_if2.h>
+
+#define _GPIO_H_LED_RED         23
+#define _GPIO_H_LED_BLUE        22
+#define _GPIO_H_LED_IR          5
+
+#define _GPIO_H_LED_RED_ON      PI_LOW
+#define _GPIO_H_LED_BLUE_ON     PI_LOW
+#define _GPIO_H_LED_IR_ON       PI_HIGH
+
+#define _GPIO_H_LED_RED_OFF     PI_HIGH
+#define _GPIO_H_LED_BLUE_OFF    PI_HIGH
+#define _GPIO_H_LED_IR_OFF      PI_LOW
+
+enum StatusLed {stat_off, stat_red, stat_blue, stat_purple};
+enum InfraLed {ir_off, ir_on};
+
+struct GPIO {
+    int _fd;
+    enum StatusLed _statusled;
+    enum InfraLed _infraled;
+};
+
+/* Initializes the GPIO object. Must be called before any other GPIO functions.
+ * A `struct GPIO` pointer must be passed to @param gpio. This structure is then
+ * initialized, and the hardware is also initialized.
+ * @returns 0 on success or a negative on failure.
+ */
+int GPIO_initialize(struct GPIO *gpio);
+
+/* Finalizes the GPIO object. Must be called before exiting the program.
+ * @returns 0 on success or a negative on failure.
+ */
+int GPIO_finalize(struct GPIO *gpio);
+
+/* Sets the product's status LED. See the `StatusLed` enum for color options.
+ * The function takes a `struct GPIO` @param gpio to hold the object's state. 
+ * The struct must be already initialized. The @param color is used to specify 
+ * the color. @returns 0 on success or a pigpiod_if2 error code on failure.
+ */
+int GPIO_set_StatusLED(struct GPIO *gpio, enum StatusLed color);
+
+/* Gets the product's status LED. See the `StatusLed` enum for color options.
+ * The function takes a `struct GPIO` on @param gpio and this is used to return
+ * the status to the `statusled` member. The struct must be already initialized.
+ * @returns 0 on success or a pigpiod_if2 error code on failure.
+ */
+int GPIO_get_StatusLED(struct GPIO *gpio);
+
+/* Sets the product's infrared LED. See the `InfraLed` enum for color options.
+ * The function takes a `struct GPIO` @param gpio to hold the object's state. 
+ * The struct must be already initialized. The @param state is used to specify 
+ * the color. @returns 0 on success or a pigpiod_if2 error code on failure.
+ */
+int GPIO_set_InfraLED(struct GPIO *gpio, enum InfraLed state);
+
+/* Gets the product's infrared LED. See the `InfraLed` enum for options.
+ * The function takes a `struct GPIO` on @param gpio and this is used to return
+ * the status to the `infraled` member. The struct must be already initialized.
+ * @returns 0 on success or a pigpiod_if2 error code on failure.
+ */
+int GPIO_get_InfraLED(struct GPIO *gpio);
+
+#endif
---
+++ acc-control/source/gpio.c
@@ -0,0 +1,95 @@
+#include <string.h>
+#include <syslog.h>
+#include <pigpiod_if2.h>
+#include "gpio.h"
+
+int GPIO_initialize(struct GPIO *gpio)
+{
+    gpio = memset(gpio, 0, sizeof(*gpio));
+    gpio->_fd = pigpio_start(NULL, NULL);
+    if(gpio->_fd < 0) {
+        syslog(LOG_ERR, "Failed to open pigpio interface.");
+    }
+    return gpio->_fd;
+}
+
+int GPIO_finalize(struct GPIO *gpio)
+{
+    pigpio_stop(gpio->_fd);
+    gpio = memset(gpio, -1, sizeof(*gpio));
+    return 0;
+}
+
+int GPIO_set_StatusLED(struct GPIO *gpio, enum StatusLed color)
+{
+    int r = 0;
+    int gpioret[2] = {0,0};
+    switch(color) {
+        default:
+        case stat_off:
+            gpioret[0] = 
+                gpio_write(gpio->_fd, _GPIO_H_LED_RED, _GPIO_H_LED_RED_OFF);
+            gpioret[1] = 
+                gpio_write(gpio->_fd, _GPIO_H_LED_BLUE, _GPIO_H_LED_BLUE_OFF);
+            break;
+        case stat_red:
+            gpioret[0] = 
+                gpio_write(gpio->_fd, _GPIO_H_LED_RED, _GPIO_H_LED_RED_ON);
+            gpioret[1] = 
+                gpio_write(gpio->_fd, _GPIO_H_LED_BLUE, _GPIO_H_LED_BLUE_OFF);
+            break;
+        case stat_blue:
+            gpioret[0] = 
+                gpio_write(gpio->_fd, _GPIO_H_LED_RED, _GPIO_H_LED_RED_OFF);
+            gpioret[1] = 
+                gpio_write(gpio->_fd, _GPIO_H_LED_BLUE, _GPIO_H_LED_BLUE_ON);
+            break;
+        case stat_purple:
+            gpioret[0] = 
+                gpio_write(gpio->_fd, _GPIO_H_LED_BLUE, _GPIO_H_LED_BLUE_ON);
+            gpioret[1] = 
+                gpio_write(gpio->_fd, _GPIO_H_LED_RED, _GPIO_H_LED_RED_ON);
+            break;
+    }
+    for(uint8_t i=0; i < sizeof(gpioret)/sizeof(gpioret[0]); i++) {
+        if(gpioret[i] != 0) {
+            r = gpioret[i];
+            syslog(LOG_ERR, "Got %i trying to gpio_write the status LED", r);
+        }
+        break;
+    }
+    return r;
+}
+
+int GPIO_get_StatusLED(struct GPIO *gpio)
+{
+    syslog(LOG_ERR, "GPIO_get_StatusLED not implemented");
+    return -1;
+}
+
+int GPIO_set_InfraLED(struct GPIO *gpio, enum InfraLed state)
+{
+    int r = 0;
+    int gpioret = 0;
+    switch(state) {
+        default:
+        case ir_off:
+            gpioret = gpio_write(gpio->_fd, _GPIO_H_LED_IR, _GPIO_H_LED_IR_OFF);
+            break;
+        case ir_on:
+            gpioret = gpio_write(gpio->_fd, _GPIO_H_LED_IR, _GPIO_H_LED_IR_ON);
+            break;
+    }
+    if(gpioret !=0) {
+        r = gpioret;
+        syslog(LOG_ERR, "Got %i trying to gpio_write the infrared LED",r);
+    }
+    return r;
+}
+
+int GPIO_get_InfraLED(struct GPIO *gpio)
+{
+    syslog(LOG_ERR, "GPIO_get_InfraLED not implemented");
+    return -1;
+}
+
