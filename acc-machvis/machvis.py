bd0e5401e6e35bd07774ffd729a377de646c1285
Ivan Veloz <devel@ivanveloz.com>
2024-05-30 16:10:34 -0400

    Implemented machvis with UDP streaming and normalization

--- acc-machvis/machvis.py
+++ acc-machvis/machvis.py
@@ -11,9 +11,12 @@ images = []
 
 class AccImage:
     def __init__ (self, srcimg: cv2.Mat):
-        srcrot = cv2.rotate(srcimg, cv2.ROTATE_90_CLOCKWISE)
-        srchsv = cv2.cvtColor(srcrot, cv2.COLOR_BGR2HSV)
+        self.src = srcimg
+        self._magicmarkers = (100, 101, 102, 103) 
+        # These are the codes stuck to the AC unit, clockwise from top left
 
+
+    def _detectMarkers(self):
         # Aruco detection
         magicmarkers = (100, 101, 102, 103) # markers in OpenCV clockwise order
         dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_250)
@@ -21,28 +24,38 @@ class AccImage:
         detector = cv2.aruco.ArucoDetector(dict, detectorparams)
 
         (cornersarray, idarray, rejectedImgPointsarray) = \
-            detector.detectMarkers(srcrot)
+            detector.detectMarkers(self._src)
+        self._markers = (cornersarray, idarray, rejectedImgPointsarray)
         
-        fourcorners = {}
 
-        if idarray.any:
+    def _detectCorners(self):
+        (cornersarray, idarray, rejectedImgPointsarray) = self._markers        
+        fourcorners = {}
+        if idarray is not None:
             for n,id in enumerate(idarray):
-                for m in magicmarkers:
+                for m in self._magicmarkers:
                     if(id == m):
                         fourcorners[m] = cornersarray[n]
             if len(fourcorners) != 4:
-                print('Could not find all aruco markers!')
-                print(fourcorners)
+                print('Could not find all aruco markers! Found:')
+                print(fourcorners.keys())
+                self._corners = {}
+                return
+            print('Found all aruco markers. Found:')
+            print(fourcorners.keys())
+            self._corners = fourcorners
         else:
-            print('Could not find aruco number {}'.format(magicmarkers))
-        
+            print('Could not find aruco')
+            self._corners = {}
+
+    def _normalizePerspective(self):
         # Perspective transformation
-        if len(fourcorners) == 4:
+        if len(self._corners) == 4:
             points1 = np.float32([
-                fourcorners[magicmarkers[0]][0][0],
-                fourcorners[magicmarkers[1]][0][1],
-                fourcorners[magicmarkers[2]][0][2],
-                fourcorners[magicmarkers[3]][0][3],
+                self._corners[self._magicmarkers[0]][0][0],
+                self._corners[self._magicmarkers[1]][0][1],
+                self._corners[self._magicmarkers[2]][0][2],
+                self._corners[self._magicmarkers[3]][0][3],
                 ])
             points2 = np.float32([
                 [0,0],
@@ -54,58 +67,57 @@ class AccImage:
             ap1 = points1#[0:3]
             ap2 = points2#[0:3]
             M = cv2.getPerspectiveTransform(ap1, ap2)
-            rows,cols,ch = srcrot.shape
-            dst = cv2.warpPerspective(srcrot, M, (dimensions))
-            cv2.imshow("Original", srcrot)
-            cv2.imshow("Transformed", dst)
-            cv2.waitKey()
-            cv2.destroyWindow("Original")
-            cv2.destroyWindow("Transformed")
-            # TODO: store the images as transformed with perspective correction.
+            rows,cols,ch = self._src.shape
+            normalized = cv2.warpPerspective(self._src, M, (dimensions))
+            self._norm = normalized
         else:
-            cv2.imshow("NOT Transformed, could not find Aruco", srcrot)
-            cv2.waitKey()
-            cv2.destroyWindow("NOT Transformed, could not find Aruco")
+            self._norm = None
+
+    def _processNorm(self):
+        self._detectMarkers()
+        self._detectCorners()
+        self._normalizePerspective()
+
+    def _setsrc(self, srcimg):
+        self._src = cv2.rotate(srcimg, cv2.ROTATE_90_CLOCKWISE)
+        self._normIsProcessed = False
     
-        srchsv = cv2.cvtColor(srcrot, cv2.COLOR_BGR2HSV)
+    def _getsrc(self):
+        return self._src
+    
+    def _getnorm(self):
+        if not self._normIsProcessed:
+            self._processNorm()
+            self._normIsProcessed = True
+        return self._norm
 
-        # Masking
-        #These masks are good for the LEDs, iffy for the 7 segment display
-        lower_nhue = np.array([85,0,0])              # negated hue
-        upper_nhue = np.array([180,255,255])         # negated hue
-        lower_brightness = np.array([0,30,70])       # allowed brightness
-        upper_brightness = np.array([255,255,255])   # allowed brightness
+    @classmethod
+    def getnorm(srcimg: cv2.Mat):
+        ai = AccImage(srcimg)
+        return ai.norm
 
-        hue_m = cv2.bitwise_not(cv2.inRange(srchsv, lower_nhue, upper_nhue))
-        sat_m = cv2.inRange(srchsv,lower_brightness, upper_brightness)
-        mask = cv2.bitwise_and(hue_m, sat_m)
-        srcmaskedled = cv2.bitwise_and(srcrot, srcrot, mask=mask)
-
-        self.sourceImage: cv2.Mat = srcrot
-        self.hsvImage: cv2.Mat = srchsv
-        self.maskedLedImage: cv2.Mat = srcmaskedled
-
-    def showall(self, prefix: str = '', suffix: str = ''):
-        p,s = prefix, suffix
-        cv2.imshow("{}sourceImage{}".format(p,s), self.sourceImage)
-        cv2.imshow("{}hsvImage{}".format(p,s), self.hsvImage)
-        cv2.imshow("{}maskedLedImage{}".format(p,s), self.maskedLedImage)
-        self._showallxfix = (p,s)
-
-    def destroyall(self):
-        if(self._showallxfix):
-            p, s = self._showallxfix
-            cv2.destroyWindow("{}sourceImage{}".format(p,s))
-            cv2.destroyWindow("{}hsvImage{}".format(p,s))
-            cv2.destroyWindow("{}maskedLedImage{}".format(p,s))
-            self._showallxfix = None
-            return True
-        else:
-            return False
+    src = property(fget=_getsrc, fset=_setsrc)
+    norm = property(fget=_getnorm)
 
 
 
+cap = cv2.VideoCapture("udp://@239.0.0.10:5000", cv2.CAP_FFMPEG)
 
+while(cap.isOpened()):
+    ret, frame = cap.read()
+    if(ret == True):
+        ai = AccImage(frame)
+        normframe = ai.norm
+        if normframe is None:
+            normframe = np.zeros(shape=[512,512,3], dtype=np.uint8)
+        cv2.imshow("Live feed", frame)
+        cv2.imshow("Normalized live feed", normframe)
+        if cv2.waitKey(1) == ord('q'):
+            break
+
+cap.release()
+cv2.destroyAllWindows()
+exit(0)
 
 images = []
 
