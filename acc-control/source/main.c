a9c88649d32c9bf67c1c4135a6e667a0c0462a28
Ivan Veloz <devel@ivanveloz.com>
2024-06-21 17:42:45 +0000

    Created control class. Moved publish thread to control class.

--- acc-control/source/main.c
+++ acc-control/source/main.c
@@ -6,12 +6,15 @@
 #include "infrared.h"
 #include "mqtt.h"
 #include "machvis.h"
+#include "control.h"
 
 #define LEDSLEEP    500000
 
 struct mainthreads_st {
     pthread_t machvis;
-    pthread_t mqtt;
+    pthread_t control_publish;
+    pthread_t control_listen;
+    pthread_t control_loop;
 };
 
 int main() {
@@ -22,10 +25,11 @@ int main() {
     struct mqtt_st mqtt;
     struct machvis_st mv;
     struct mainthreads_st mt;
+    struct control_st control;
 
+    
     r = machvis_initialize(&mv);
     assert(r == 0);
-
     pthread_create(&mt.machvis, NULL, machvis_receive, &mv);
 
     r = mqtt_initialize(&mqtt, &mv);
@@ -33,20 +37,32 @@ int main() {
     r = mqtt_publish_unit_ping(&mqtt);
     assert(r == 0);
 
-    pthread_create(&mt.mqtt, NULL, mqtt_publish, &mqtt);
+    r = control_initialize(&control, &mqtt, &mv);
+    assert(r == 0);
+    pthread_create(&mt.control_publish, NULL, control_publish, &control);
+    pthread_create(&mt.control_listen, NULL, control_listen, &control);
+    pthread_create(&mt.control_loop, NULL, control_loop, &control);
 
     // This pause could be a loop that watches over the threads instead
     pause();
     mqtt.publish = false;
     mv.receive = false;
+    control.listen = false;
+    control.publish = false;
 
-    pthread_join(&mt.mqtt, NULL);
+    pthread_join(&mt.control_loop, NULL);
+    pthread_join(&mt.control_listen, NULL);
+    pthread_join(&mt.control_publish, NULL);
+    pthread_join(&mt.machvis, NULL);
+
+    r = control_finalize(&control);
+    assert (r == 0);
+
     r = mqtt_disconnect(&mqtt);
     assert(r == 0);
     r = mqtt_finalize(&mqtt);
     assert(r == 0);
     
-    pthread_join(&mt.machvis, NULL);
     r = machvis_close(&mv);
     assert(r == 0 || errno == EBADF);
     r = machvis_finalize(&mv);
