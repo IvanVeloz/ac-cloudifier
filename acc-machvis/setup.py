48dc9efe763c5729510f37e3dec9c2c06fafaba2
Ivan Veloz <devel@ivanveloz.com>
2024-06-22 23:31:37 +0000

    Created setuptools setup

---
+++ acc-machvis/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='acc-machvis',
+    version='0.1.1',
+    description='acc-machvis',
+    author='Ivan Veloz',
+    url='https://www.github.com/IvanVeloz/ac-cloudifier/',
+    py_modules=['accvis', 'machvis', 'fakevis'],
+    entry_points={
+        'console_scripts': [
+            'machvis=machvis:main',
+            'fakevis=fakevis:main',
+        ],
+    },
+    install_requires=[
+        'numpy>=1.24.4',
+        'opencv-python>=4.8.0',
+    ],
+    python_requires='>=3.8.10',
+)
