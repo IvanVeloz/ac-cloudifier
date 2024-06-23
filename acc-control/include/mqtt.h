03c9b78a73800f18bdfbcad923fc252f55d70c76
Ivan Veloz <devel@ivanveloz.com>
2024-06-23 18:07:41 +0000

    Added missing mqtt_publish_unit_ping

--- acc-control/include/mqtt.h
+++ acc-control/include/mqtt.h
@@ -29,6 +29,7 @@ int mqtt_connect(struct mqtt_st * mqtt);
 int mqtt_disconnect(struct mqtt_st * mqtt);
 void *mqtt_publish(void *args);
 int mqtt_publish_panel_state(struct mqtt_st * mqtt, struct machvis_st * mv);
+int mqtt_publish_unit_ping(struct mqtt_st * mqtt);
 char * mqtt_listen_command(struct mqtt_st *mqtt);
 
 #endif /* #ifndef _MQTT_H_ */
